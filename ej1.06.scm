4be1275d5c93630f08033cf80bfcfce64690557e
yihahoi <yihahoi@localhost.localdomain>
2015-11-14 23:33:25 -0300

    nuevo orden

--- ej1.06.scm
+++
@@ -1,6 +0,0 @@
-#lang planet neil/sicp
-
-;el problema esta en que al llamar al procedimiento "square-iter",
-;al evaluar el procedimiento new-if, se evaluara si o si la tercera expresion, correspondiente al else:
-;(sqrt-iter (improve guess x) x)
-;entrando en un bucle infinito.
