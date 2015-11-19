007926a64c1ca277fc90f5ec9024e151f2e5f7ee
yihahoi <yihahoi@localhost.localdomain>
2015-11-19 02:31:23 -0300

    pag 99 <:D

---
+++ Cap2/ej2.05.scm
@@ -0,0 +1,42 @@
+#lang planet neil/sicp
+
+; ------------- representacion -------------
+
+; cons
+(define (cons a b)
+  (define (cons2 a b acum)
+    (cond ((> a 0) (cons2 (- a 1) b (* 2 acum)))
+          ((> b 0) (cons2 a (- b 1) (* 3 acum)))
+          (else acum)))
+  (cons2 a b 1))
+
+; desfragmentar
+(define (desfragmentar x base acum)
+  (cond ((= 0 (remainder x base))
+         (desfragmentar (/ x base) 
+                        base 
+                        (+ acum 1)))
+        (else acum)))
+
+; car
+(define (car x)
+  (desfragmentar x 2 0))
+
+; cdr
+(define (cdr x)
+  (desfragmentar x 3 0))
+
+; ------------------ test ------------------
+
+(display "a: 15")
+(newline)
+(display "b: 13")
+(newline)
+(define x (cons 15 13))
+(display "x = (cons a b) = ")
+(display x)
+(newline)
+(display "(car x) = ")
+(car x)
+(display "(cdr x) = ")
+(cdr x)
---
+++ Cap2/ej2.06.scm
@@ -0,0 +1,72 @@
+#lang planet neil/sicp
+
+; ------------------ Numerales de Church ------------------
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+; -------------------------- one --------------------------
+
+; zero es un procedimiento que pide un argumento f y devuelve 
+; otro procedimiento que pide un argumento x y devuelve x.
+; tambien puede escribirse como:
+; (define (zero f) (lambda (x) x))
+; en este caso (zero f) devuelve un procedimiento identidad
+
+; para add-1: 
+; (add-1 zero)
+; (lambda (f) (lambda (x) (f ((n f) x))))
+; (lambda (f) (lambda (x) (f ((zero f) x))))
+; (lambda (f) (lambda (x) (f ((lambda (x) x) x))))
+; (lambda (f) (lambda (x) (f x)))
+; (lambda (f) (lambda (x) (f x)))
+
+; es decir, (add-1 zero) devuelve un procedimiento que pide 
+; como argumento una funcion f, y esta devuelve otro procedimiento
+; que pide un argumento x, y luego aplica esta funcion f al argumento x.
+; de esta forma se puede representar 'one' como:
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+; otra forma de definir one es
+; (define (one f) (lambda (x) (f x))) 
+
+; -------------------------- two --------------------------
+
+; para obtener 'two', podemos descomponer (add-1 one)
+; (add-1 one)
+; (lambda (f) (lambda (x) (f ((n f) x))))
+; (lambda (f) (lambda (x) (f ((one f) x))))
+; (lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
+; (lambda (f) (lambda (x) (f (f x))))
+
+; es decir, (add-1 one) devuelve un procedimiento que pide 
+; como argumento una funcion f, y esta devuelve otro procedimiento
+; que pide un argumento x, y luego aplica 2 veces esta funcion f al argumento x.
+; de esta forma se puede representar 'two' como:
+
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+; otra forma de definir two es
+; (define (two f) (lambda (x) (f (f x))))
+
+; -------------------------- + --------------------------
+
+; la idea es definir un procedimiento que permita representar lo siguiente:
+; (define (three f) ((+ one two) f))
+; (define (four f) ((+ one three) f))
+; (define (five f) ((+ two three) f))
+; y asi sucesivamente ... para esto, se puede notar que es posible
+; realizar composicion de funciones del tipo (f (g x)) ya que se
+; tienen procedimientos que reciben y devuelven procedimientos. 
+
+(define (+ f g) (lambda (x) (f (g x))))
+
+; ------------------------ extra ------------------------
+
+(define (three f) ((+ one two) f))
+(define (four f) ((+ one three) f))
+(define (five f) ((+ two three) f))
+
