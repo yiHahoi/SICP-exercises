e35a55b31fab44be4a547681efbca0ef124b1e85
yihahoi <yihahoi@localhost.localdomain>
2015-12-04 03:46:04 -0300

    fringe

---
+++ Cap2/ej2.28.scm
@@ -0,0 +1,16 @@
+#lang planet neil/sicp
+
+; funcion fringe
+(define (fringe lst)
+  (cond ((null? lst) nil) 
+        ((pair? (car lst)) (append (fringe (car lst)) 
+                                   (fringe (cdr lst))))
+        (else (append (list (car lst)) 
+                      (fringe (cdr lst))))))
+
+
+; tests
+(define x (list (list 1 2) (list 3 4)))
+(display (fringe x))
+(newline)
+(display (fringe (list x x)))
