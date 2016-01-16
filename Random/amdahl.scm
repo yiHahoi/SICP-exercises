444718fb172c423235a8aeb5cc97335d4b84caa3
yihahoi <yihahoi@localhost.localdomain>
2016-01-16 15:00:25 -0300

    comentario

--- Random/amdahl.scm
+++ Random/amdahl.scm
@@ -5,11 +5,11 @@
 ; frac  : fraccion de tiempo de ejecucion a paralelizar
 ; cores : numero de nucleos (aprox. la aceleracion de la fraccion paralelizable)
 ;
-;    			1
-;	s =	-------------------
-;		(1 - frac) + frac
-;					-------
-;					 cores
+;                       1
+;      s =     ------------------
+;              (1 - frac) + frac
+;                          -------
+;                           cores
 
 (define (amdahl frac cores)
 	(/ 1 (+ (- 1 frac) (/ frac cores))))
